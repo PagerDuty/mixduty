df5acbeb7f606929b8d6175edc3ee5cb725e8df7
Ian Minoso <iminoso@gmail.com>
2018-10-22 02:30:00 -0400

    Doc client

--- lib/mixduty/client.ex
+++ lib/mixduty/client.ex
@@ -2,6 +2,15 @@ defmodule Mixduty.Client do
   alias __MODULE__
   defstruct headers: nil
 
+  @moduledoc """
+  The client used to make requests to the PagerDuty v2 API
+  """
+
+  @doc """
+  Create the client
+  #### Example
+      Mixduty.Client.new("myauthtoken")
+  """
   def new(auth) do
     %Client{
       headers: [
