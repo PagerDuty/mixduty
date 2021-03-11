4985a37a2e9c0d26105ffe80d533df16952cad7c
Daria Struts <daryastruts@gmail.com>
2021-03-11 13:49:29 +0200

    Add optional headers to client

--- lib/mixduty/client.ex
+++ lib/mixduty/client.ex
@@ -8,31 +8,33 @@ defmodule Mixduty.Client do
 
   @doc """
   Create the client
-  #### Example
+  #### Examples
       Mixduty.Client.new("myauthtoken")
+      Mixduty.Client.new("myauthtoken", [{"X-EARLY-ACCESS", "business-impact-early-access"}])
   """
-  def new(auth) do
+  def new(auth, headers \\ []) do
     %Client{
       headers: [
         {"Accept", "application/vnd.pagerduty+json;version=2"},
         {"Authorization", "Token token=#{auth}"},
         {"Content-type", "application/json"}
-      ]
+      ] ++ headers
     }
   end
 
   @doc """
   Create the client for Bearer authorization token
-  #### Example
-      Mixduty.Client.new(:bearer, "myauthtoken")
+  #### Examples
+      Mixduty.Client.new(:bearer, "bearerauthtoken")
+      Mixduty.Client.new(:bearer, "bearerauthtoken", [{"X-EARLY-ACCESS", "business-impact-early-access"}])
   """
-  def new(:bearer, auth) do
+  def new(:bearer, auth, headers \\ []) do
     %Client{
       headers: [
         {"Accept", "application/vnd.pagerduty+json;version=2"},
         {"Authorization", "Bearer #{auth}"},
         {"Content-type", "application/json"}
-      ]
+      ] ++ headers
     }
   end
 end
