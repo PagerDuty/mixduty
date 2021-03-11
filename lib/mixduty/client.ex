67037472ec51f8c88b5c5681a04badde73876eab
Daria Struts <daryastruts@gmail.com>
2021-03-11 15:56:39 +0200

    Remove optional headers to client

--- lib/mixduty/client.ex
+++ lib/mixduty/client.ex
@@ -8,33 +8,31 @@ defmodule Mixduty.Client do
 
   @doc """
   Create the client
-  #### Examples
+  #### Example
       Mixduty.Client.new("myauthtoken")
-      Mixduty.Client.new("myauthtoken", [{"X-EARLY-ACCESS", "business-impact-early-access"}])
   """
-  def new(auth, headers \\ []) do
+  def new(auth) do
     %Client{
       headers: [
         {"Accept", "application/vnd.pagerduty+json;version=2"},
         {"Authorization", "Token token=#{auth}"},
         {"Content-type", "application/json"}
-      ] ++ headers
+      ]
     }
   end
 
   @doc """
   Create the client for Bearer authorization token
-  #### Examples
+  #### Example
       Mixduty.Client.new(:bearer, "bearerauthtoken")
-      Mixduty.Client.new(:bearer, "bearerauthtoken", [{"X-EARLY-ACCESS", "business-impact-early-access"}])
   """
-  def new(:bearer, auth, headers \\ []) do
+  def new(:bearer, auth) do
     %Client{
       headers: [
         {"Accept", "application/vnd.pagerduty+json;version=2"},
         {"Authorization", "Bearer #{auth}"},
         {"Content-type", "application/json"}
-      ] ++ headers
+      ]
     }
   end
 end
