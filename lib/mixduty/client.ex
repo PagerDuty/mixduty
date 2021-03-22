062dd14cf5fbb59873327090f0a6d675d79eca61
Daria Struts <daryastruts@gmail.com>
2021-03-22 15:06:05 +0200

    Add client with additional headers (#14)

--- lib/mixduty/client.ex
+++ lib/mixduty/client.ex
@@ -27,12 +27,22 @@ defmodule Mixduty.Client do
       Mixduty.Client.new("bearerauthtoken", [type: :bearer])
   """
   def new(auth, type: :bearer) do
+    new(auth, type: :bearer, headers: [])
+  end
+
+  @doc """
+  Internal use only
+  Create the client for Bearer authorization token with additional headers
+  #### Example
+      Mixduty.Client.new("bearerauthtoken", [type: :bearer, headers: [{"X-EARLY-ACCESS", "business-impact-early-access"}])
+  """
+  def new(auth, type: :bearer, headers: additional_headers) do
     %Client{
       headers: [
         {"Accept", "application/vnd.pagerduty+json;version=2"},
         {"Authorization", "Bearer #{auth}"},
         {"Content-type", "application/json"}
-      ]
+      ] ++ additional_headers
     }
   end
 end
